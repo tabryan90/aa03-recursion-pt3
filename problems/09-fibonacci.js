a99bdc4654580b4aa5fc108fe327ff3985086e3f
Alex Bryan <tabryan1990@gmail.com>
2024-02-22 22:33:05 -0500

    problem 09 solved

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -19,10 +19,24 @@ fibonacci(4); // 3
 fibonacci(10); // 55
 ***********************************************************************/
 
+
 function fibonacci(n) {
-  // Your code here 
+
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
 }
-  
+
+fibonacci(1); // 1
+fibonacci(2); // 1
+fibonacci(3); // 2
+fibonacci(4); // 3
+fibonacci(10); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;
-  
