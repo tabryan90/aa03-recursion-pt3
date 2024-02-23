668d1a66487ec7ca7d83d98308061387125af6cb
swan3986 <swan3986@gmail.com>
2024-02-22 20:40:35 -0800

    12 complete/working on 11

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a recursive function called `flatten` that takes a single array with
-any number of nested arrays and returns and array with all the nested
+any number of nested arrays and returns an array with all the nested
 contents on one level.
 
 Do not use the built in Array.flat.
@@ -13,9 +13,24 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here 
+  if (!arr.length) {
+    return [];
+  }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let el1 = arr[i];
+    newArr.push(el1);
+
+
+  }
+
+  console.log(newArr);
 }
-  
+
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -10,10 +10,22 @@ isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
-
+debugger
 function isSorted(arr) {
-  // Your code here 
+  if (arr.length === 1) {
+    return true;
+  }
+
+  if (arr[0] < arr[1]) {
+    return isSorted(arr.slice(1))
+  } else {
+    return false;
+  }
+
 }
+
+// console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
